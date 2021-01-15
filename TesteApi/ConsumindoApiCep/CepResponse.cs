3e49652ab1c3b36a38af4cbea5f7c77b85faed78
Marcos André <marco@DESKTOP-AM7V1K8>
2021-01-15 11:50:15 -0300

    Criada as classes,instalado o pacote Refit

---
+++ TesteApi/ConsumindoApiCep/CepResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ConsumindoApiCep
+{
+    class CepResponse
+    {   
+        [JsonProperty("cep")]
+        public string Cep { get; set; }
+        [JsonProperty("bairro")]
+        public string Bairro { get; set; }
+        [JsonProperty("rua")]
+        public string Rua { get; set; }
+        [JsonProperty("logradouro")]
+        public string Logradouro { get; set; }
+        [JsonProperty("localidade")]
+        public string Localidade { get; set; }
+        [JsonProperty("ibge")]
+        public string Ibge { get; set; }
+        [JsonProperty("complemento")]
+        public string Complemento  { get; set; }
+        [JsonProperty("uf")]
+        public string Uf { get; set; }
+
+    }
+}
